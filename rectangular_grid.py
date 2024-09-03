0c2a3ea8e6966d3e623ad201ec69611626cfba59
Sandro M. Roch <180171033+sandro-roch@users.noreply.github.com>
2024-09-03 12:58:06 +0200

    Slight changes in rectangular_grid.py: Loop for different values of k, display python version number, limit output to every 1000th line etc.

--- rectangular_grid.py
+++ rectangular_grid.py
@@ -9,6 +9,10 @@
 
 import itertools
 import random
+import sys
+
+
+print("PYTHON VERSION:\n" + sys.version)
 
 
 def expected_weight(p_strips, p_boundary_front, p_boundary_left, p_boundary_right, p_boundary_back):
@@ -101,102 +105,105 @@ def expected_weight(p_strips, p_boundary_front, p_boundary_left, p_boundary_righ
     return total_weight_of_fillings / total_number_of_fillings
 
 
-# Change this value for different values of k.
-k = 4
+for k in range(2, 4):
 
-# Step 1: Compute all k-strips, i.e. k-heights on a path of length 4
-print("Computing k-strips...")
+    print("CASE k = " + str(k))
 
-k_strips = []
-for strip in itertools.product(range(k + 1), repeat=4):
-    is_valid_k_strip = True
-    for i in range(3):
-        if abs(strip[i] - strip[i + 1]) > 1:
-            is_valid_k_strip = False
-    if is_valid_k_strip:
-        k_strips.append(strip)
+    # Step 1: Compute all k-strips, i.e. k-heights on a path of length 4
+    print("Computing k-strips...")
 
-# Step 2: Compute all possible boundary restrictions, encoded as 16-tuples in counterclockwise order starting with
-# front side
-print("Computing boundaries...")
+    k_strips = []
+    for strip in itertools.product(range(k + 1), repeat=4):
+        is_valid_k_strip = True
+        for i in range(3):
+            if abs(strip[i] - strip[i + 1]) > 1:
+                is_valid_k_strip = False
+        if is_valid_k_strip:
+            k_strips.append(strip)
 
-boundary_constraints = []
-for sides in itertools.product(k_strips, repeat=4):
-    # Two boundary vertices that have distance 2 can only differ by at most two. Hence, we can ignore the boundary
-    # constraints in which this property does not hold.
-    if abs(sides[0][3] - sides[1][0]) > 2:
-        continue
-    if abs(sides[1][3] - sides[2][0]) > 2:
-        continue
-    if abs(sides[2][3] - sides[3][0]) > 2:
-        continue
-    if abs(sides[3][3] - sides[0][0]) > 2:
-        continue
-    boundary_constraints.append(sides[0] + sides[1] + sides[2] + sides[3])
+    # Step 2: Compute all possible boundary restrictions, encoded as 16-tuples in counterclockwise order starting with
+    # front side
+    print("Computing boundaries...")
+
+    boundary_constraints = []
+    for sides in itertools.product(k_strips, repeat=4):
+        # Two boundary vertices that have distance 2 can only differ by at most two. Hence, we can ignore the boundary
+        # constraints in which this property does not hold.
+        if abs(sides[0][3] - sides[1][0]) > 2:
+            continue
+        if abs(sides[1][3] - sides[2][0]) > 2:
+            continue
+        if abs(sides[2][3] - sides[3][0]) > 2:
+            continue
+        if abs(sides[3][3] - sides[0][0]) > 2:
+            continue
+        boundary_constraints.append(sides[0] + sides[1] + sides[2] + sides[3])
 
 
-# Step 3: Consider all cover relations where the differing vertex is either the first or the second vertex of the front
-# boundary (other cases are symmetric). Compute the expected weight difference and take the maximum over all of these.
-print("Computing expected weight differences on cover relations...")
+    # Step 3: Consider all cover relations where the differing vertex is either the first or the second vertex of the front
+    # boundary (other cases are symmetric). Compute the expected weight difference and take the maximum over all of these.
+    print("Computing expected weight differences on cover relations...")
 
-# We will maximize over all boundary_constraints. Shuffling their order does not affect the final result. One could
-# actually remove it. However, if we shuffle their order randomly one can quickly "see" or guess the value that is
-# being approached without the need running the process till it terminates. However, for the proof in our article we
-# only use the final result value.
-random.shuffle(boundary_constraints)
+    # We will maximize over all boundary_constraints. Shuffling their order does not affect the final result. One could
+    # actually remove it. However, if we shuffle their order randomly one can quickly "see" or guess the value that is
+    # being approached without the need running the process till it terminates. However, for the proof  of rapid mixing
+    # in the case of k = 2, 3 in our article we only use the final result value. However, for lower bounding the block
+    # divergence in the case of k = 4, we do interrupt the algorithm.
+    random.shuffle(boundary_constraints)
 
-max_block_divergence = 0
+    max_block_divergence = 0
 
-for r in range(len(boundary_constraints)):
+    for r in range(len(boundary_constraints)):
 
-    boundary_constraint = boundary_constraints[r]
-    print("Processing " + str(r) + "th boundary constraint out of " + str(len(boundary_constraints)) + " in total. Max block divergence found so far: " + str(max_block_divergence))
+        boundary_constraint = boundary_constraints[r]
+        if (r % 1000) == 0:
+            print("Processing " + str(r) + "th boundary constraint out of " + str(len(boundary_constraints)) + " in total. Max block divergence found so far: " + str(max_block_divergence))
 
-    # split counterclockwise indexed 16-tuple into front, left, right and back side.
+        # split counterclockwise indexed 16-tuple into front, left, right and back side.
 
-    boundary_front = boundary_constraint[0:4]
-    boundary_right = boundary_constraint[4:8]
-    boundary_back = boundary_constraint[11:7:-1]
-    boundary_left = boundary_constraint[15:11:-1]
+        boundary_front = boundary_constraint[0:4]
+        boundary_right = boundary_constraint[4:8]
+        boundary_back = boundary_constraint[11:7:-1]
+        boundary_left = boundary_constraint[15:11:-1]
 
-    # augment 0-th front vertex if possible
+        # augment 0-th front vertex if possible
 
-    if boundary_front[0] != k:
-        augmented_boundary_front_as_list = list(boundary_front)
-        augmented_boundary_front_as_list[0] += 1
-        augmented_boundary_front = tuple(augmented_boundary_front_as_list)
+        if boundary_front[0] != k:
+            augmented_boundary_front_as_list = list(boundary_front)
+            augmented_boundary_front_as_list[0] += 1
+            augmented_boundary_front = tuple(augmented_boundary_front_as_list)
 
-        if not (abs(augmented_boundary_front[0] - augmented_boundary_front[1]) > 1) \
-                and not (abs(augmented_boundary_front[0] - boundary_left[0]) > 2):
-            # is still a valid boundary after augmentation
+            if not (abs(augmented_boundary_front[0] - augmented_boundary_front[1]) > 1) \
+                    and not (abs(augmented_boundary_front[0] - boundary_left[0]) > 2):
+                # is still a valid boundary after augmentation
 
-            expected_weight_without_augmentation = expected_weight(k_strips, boundary_front, boundary_left,
-                                                                   boundary_right, boundary_back)
-            expected_weight_with_augmentation = expected_weight(k_strips, augmented_boundary_front, boundary_left,
-                                                                boundary_right, boundary_back)
+                expected_weight_without_augmentation = expected_weight(k_strips, boundary_front, boundary_left,
+                                                                       boundary_right, boundary_back)
+                expected_weight_with_augmentation = expected_weight(k_strips, augmented_boundary_front, boundary_left,
+                                                                    boundary_right, boundary_back)
 
-            expected_weight_difference = expected_weight_with_augmentation - expected_weight_without_augmentation
-            if expected_weight_difference > max_block_divergence:
-                max_block_divergence = expected_weight_difference
+                expected_weight_difference = expected_weight_with_augmentation - expected_weight_without_augmentation
+                if expected_weight_difference > max_block_divergence:
+                    max_block_divergence = expected_weight_difference
 
-    # augment 1-th front vertex if possible
+        # augment 1-th front vertex if possible
 
-    if boundary_front[1] != k:
-        augmented_boundary_front_as_list = list(boundary_front)
-        augmented_boundary_front_as_list[1] += 1
-        augmented_boundary_front = tuple(augmented_boundary_front_as_list)
+        if boundary_front[1] != k:
+            augmented_boundary_front_as_list = list(boundary_front)
+            augmented_boundary_front_as_list[1] += 1
+            augmented_boundary_front = tuple(augmented_boundary_front_as_list)
 
-        if not (abs(augmented_boundary_front[1] - augmented_boundary_front[2]) > 1) \
-                and not (abs(augmented_boundary_front[0] - augmented_boundary_front[1]) > 1):
-            # is still a valid boundary after augmentation
+            if not (abs(augmented_boundary_front[1] - augmented_boundary_front[2]) > 1) \
+                    and not (abs(augmented_boundary_front[0] - augmented_boundary_front[1]) > 1):
+                # is still a valid boundary after augmentation
 
-            expected_weight_without_augmentation = expected_weight(k_strips, boundary_front, boundary_left,
-                                                                   boundary_right, boundary_back)
-            expected_weight_with_augmentation = expected_weight(k_strips, augmented_boundary_front, boundary_left,
-                                                                boundary_right, boundary_back)
+                expected_weight_without_augmentation = expected_weight(k_strips, boundary_front, boundary_left,
+                                                                       boundary_right, boundary_back)
+                expected_weight_with_augmentation = expected_weight(k_strips, augmented_boundary_front, boundary_left,
+                                                                    boundary_right, boundary_back)
 
-            expected_weight_difference = expected_weight_with_augmentation - expected_weight_without_augmentation
-            if expected_weight_difference > max_block_divergence:
-                max_block_divergence = expected_weight_difference
+                expected_weight_difference = expected_weight_with_augmentation - expected_weight_without_augmentation
+                if expected_weight_difference > max_block_divergence:
+                    max_block_divergence = expected_weight_difference
 
-print(max_block_divergence)
+    print("BLOCK DIVERGENCE IN CASE k = " + str(k) + ": " + str(max_block_divergence))
